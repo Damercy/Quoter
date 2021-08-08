910b811d60204e71706cd5ec75bb76696b87a5fb
Damercy <adhikaridayamoy@gmail.com>
2021-08-08 14:23:16 +0530

    custom snack & minor fix

--- app/src/main/java/com/dayaonweb/quoter/view/ui/browsetag/BrowseTag.kt
+++ app/src/main/java/com/dayaonweb/quoter/view/ui/browsetag/BrowseTag.kt
@@ -1,9 +1,13 @@
 package com.dayaonweb.quoter.view.ui.browsetag
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.*
 import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
@@ -13,6 +17,7 @@ import androidx.navigation.fragment.navArgs
 import com.dayaonweb.quoter.R
 import com.dayaonweb.quoter.databinding.FragmentBrowseTagBinding
 import com.dayaonweb.quoter.service.model.Quote
+import com.google.android.material.snackbar.Snackbar
 
 class BrowseTag : Fragment(), PopupMenu.OnMenuItemClickListener {
 
@@ -22,6 +27,7 @@ class BrowseTag : Fragment(), PopupMenu.OnMenuItemClickListener {
     private var totalPages = -1
     private var pageToFetch = 1
     private var currentPageCount = 0
+    private var currentQuoteNumber = 1
     private val args: BrowseTagArgs by navArgs()
 
     override fun onCreateView(
@@ -63,10 +69,11 @@ class BrowseTag : Fragment(), PopupMenu.OnMenuItemClickListener {
                 requireActivity().onBackPressed()
             }
             quoteScroller.setOnValueChangedListener { _, _, newVal ->
+                currentQuoteNumber = newVal+1
                 val currentQuote = quoteToAuthor.keys.toTypedArray()[newVal]
                 quoteTextView.text = currentQuote.content
                 authorTextView.text = quoteToAuthor.values.toTypedArray()[newVal]
-                serialTextView.text = String.format("%s", "${newVal + 1}/$currentPageCount")
+                serialTextView.text = String.format("%s", "$currentQuoteNumber/$currentPageCount")
                 quoteTextView.setTextSize(
                     TypedValue.COMPLEX_UNIT_SP,
                     if (currentQuote.length > 150) 24f else 32f
@@ -93,16 +100,16 @@ class BrowseTag : Fragment(), PopupMenu.OnMenuItemClickListener {
         }
         bi?.quoteTextView?.text = quoteToAuthor.keys.toTypedArray()[0].content
         bi?.authorTextView?.text = quoteToAuthor.values.toTypedArray()[0]
-        bi?.serialTextView?.text = String.format("%s", "1/$currentPageCount")
+        bi?.serialTextView?.text = String.format("%s", "$currentQuoteNumber/$currentPageCount")
         bi?.shareImageView?.isVisible = true
         bi?.backImageView?.isVisible = true
         bi?.quoteImageView?.isVisible = true
         bi?.optionsImageView?.isVisible = true
     }
 
-    private fun showPopup(anchorView: View){
-        val wrapper = ContextThemeWrapper(requireContext(),R.style.BasePopupMenu)
-        PopupMenu(wrapper,anchorView).apply {
+    private fun showPopup(anchorView: View) {
+        val wrapper = ContextThemeWrapper(requireContext(), R.style.BasePopupMenu)
+        PopupMenu(wrapper, anchorView).apply {
             setOnMenuItemClickListener(this@BrowseTag)
             gravity = Gravity.END
             inflate(R.menu.options_menu)
@@ -117,12 +124,35 @@ class BrowseTag : Fragment(), PopupMenu.OnMenuItemClickListener {
     }
 
     override fun onMenuItemClick(item: MenuItem?): Boolean {
-        return when(item?.itemId){
+        return when (item?.itemId) {
             R.id.copy_quote -> {
-                // copy text
+                bi?.quoteTextView?.text?.let { quote ->
+                    bi?.authorTextView?.text?.let { author ->
+                        copyText(String.format("%s\n\n~ %s", quote, author))
+                    }
+                }
                 true
             }
             else -> false
         }
     }
+
+    private fun copyText(text: String) {
+        val clipboard = requireActivity().getSystemService(ClipboardManager::class.java)
+        val clip = ClipData.newPlainText("quote", text)
+        clipboard.setPrimaryClip(clip)
+        showSnack()
+    }
+
+    private fun showSnack() {
+        val snack = Snackbar.make(requireView(), "Copied", Snackbar.LENGTH_SHORT)
+        val snackView = snack.view
+        snackView.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.black))
+        val snackText =
+            snackView.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+        snackText.typeface = ResourcesCompat.getFont(requireContext(), R.font.main_bold)
+        snack.animationMode = Snackbar.ANIMATION_MODE_SLIDE
+        snack.show()
+    }
+
 }
